b0562a016077ff121b4f7a79ed4401dbbc142262
6ubcghyt9sq <133364724+6ubcghyt9sq@users.noreply.github.com>
2023-05-12 14:30:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,11 @@ import streamlit
 import pandas
 
 streamlit.title( 'KyParents Now Healthy Diner')
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = streamlit.dataframe(my_fruit_list)
+streamlit.multiselect("Pick some fruits:" list (my_fruit_list.index))
+
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
